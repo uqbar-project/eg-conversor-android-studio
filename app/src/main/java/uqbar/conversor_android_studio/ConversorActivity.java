30c12f273d56fe61c0e735089e01e9cd78c784fa
fdodino <fernando.dodino@gmail.com>
2017-10-26 15:38:13 -0300

    Adding toast for error conversion + new video

--- app/src/main/java/uqbar/conversor_android_studio/ConversorActivity.java
+++ app/src/main/java/uqbar/conversor_android_studio/ConversorActivity.java
@@ -8,30 +8,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import uqbar.conversor_android_studio.model.Conversor;
 
 
 public class ConversorActivity extends Activity implements View.OnClickListener {
 
-        private Conversor conversor = new Conversor();
+    private Conversor conversor = new Conversor();
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_conversor);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversor);
+        findViewById(R.id.conversor_convertir).setOnClickListener(this);
+    }
 
-          //  findViewById(R.id.conversor_convertir).setOnClickListener(this);
-        }
-
-        public void onClick(View view) {
+    public void onClick(View view) {
+        try {
             String millas = ((EditText) findViewById(R.id.conversor_millas)).getText().toString();
             conversor.setMillas(Double.valueOf(millas));
             conversor.convertir();
             ((TextView) findViewById(R.id.conversor_kilometros)).setText(String.valueOf(conversor.getKilometros()));
+        } catch (NullPointerException e) {
+            Toast.makeText(this.getApplicationContext(), "Debe ingresar un valor", Toast.LENGTH_LONG).show();
+        } catch (NumberFormatException e) {
+            Toast.makeText(this.getApplicationContext(), "Debe ingresar un valor numérico", Toast.LENGTH_LONG).show();
         }
-
-    public void convertimeEsto(View view) {
-        onClick(view);
     }
+
 }
